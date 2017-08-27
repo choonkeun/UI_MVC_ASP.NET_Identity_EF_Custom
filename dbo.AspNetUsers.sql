5d6f3caec27661bfb3b0209e50229f4035c4d6c4
choonkeun <choonkeun@yahoo.com>
2017-08-27 12:33:19 -0700

    First Upload
    Connection String database is "Account.MDF"

---
+++ dbo.AspNetUsers.sql
@@ -0,0 +1,34 @@
+﻿
+DROP TABLE [dbo].[AspNetUsers] 
+GO
+
+CREATE TABLE [dbo].[AspNetUsers] (
+    [Id]                   BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Email]                NVARCHAR (256) NULL,
+    [PasswordHash]         NVARCHAR (MAX) NULL,
+	[UserRole]             SMALLINT       NULL,
+    [UserName]             NVARCHAR (256) NULL,
+    [Gender]               SMALLINT       NOT NULL DEFAULT 0,
+    [PhoneNumber]          NVARCHAR (MAX) NULL,
+    [PhoneNumberConfirmed] BIT            NOT NULL,
+    [SecondaryPhone]       NVARCHAR (MAX) NULL,
+    [Address1]             NVARCHAR (MAX) NULL,
+    [Address2]             NVARCHAR (MAX) NULL,
+    [City]                 NVARCHAR (MAX) NULL,
+    [Region]               NVARCHAR (MAX) NULL,
+    [PostalCode]           NVARCHAR (MAX) NULL,
+    [Country]              NVARCHAR (MAX) NULL,
+    [EmailConfirmed]       BIT            NOT NULL,
+    [SecurityStamp]        NVARCHAR (MAX) NULL,
+    [TwoFactorEnabled]     BIT            NOT NULL,
+    [LockoutEndDateUtc]    DATETIME       NULL,
+    [LockoutEnabled]       BIT            NOT NULL,
+    [AccessFailedCount]    INT            NOT NULL,
+    [RegisterDate]         DATETIME       NULL, 
+    [LastLoginDate]        DATETIME       NULL, 
+    CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex]
+    ON [dbo].[AspNetUsers]([UserName] ASC);
+
